f6ceb3c36b5a906dc031d418a85fe7369b01792b
anrmz <anouarmezgualli17@gmail.com>
2023-12-20 18:06:49 +0100

    message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -13,7 +13,6 @@ root.render(
     <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 reportWebVitals();
