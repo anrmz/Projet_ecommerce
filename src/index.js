f4497bb5880f3352f764e96dcd763fafcf137685
anrmz <anouarmezgualli17@gmail.com>
2023-12-25 23:04:13 +0100

    message

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter  basename="Projet-ecommerce">
+    <BrowserRouter  basename="Projet_ecommerce">
     <App />
     </BrowserRouter>
   </React.StrictMode>,
